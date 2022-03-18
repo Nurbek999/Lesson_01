0644d899ab6305767e63e1052a2d73cee1bc47e7
Amanbek Nurbek <nurbek_aman@mail.ru>
2022-03-18 22:11:57 +0600

    соplfk

---
+++ script.js
@@ -0,0 +1,8 @@
+const nameProduct = 'Apple';
+const countProduct = 100;
+const categoryProduct = 'Fruits';
+const costProduct = 500;
+const totalSumProduct = countProduct * costProduct;
+
+console.log('Product name: ', nameProduct);
+console.log('Total sum: ', totalSumProduct);
