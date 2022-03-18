2bc3ec9aa08da8660f80d14e4af02c4a2660c70a
Amanbek Nurbek <nurbek_aman@mail.ru>
2022-03-18 22:18:06 +0600

    Добавил новый товар

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+'use strict'
+{
 const nameProduct = 'Apple';
 const countProduct = 100;
 const categoryProduct = 'Fruits';
@@ -6,3 +8,17 @@ const totalSumProduct = countProduct * costProduct;
 
 console.log('Product name: ', nameProduct);
 console.log('Total sum: ', totalSumProduct);
+}
+
+{
+    const nameProduct = 'Mac';
+    const countProduct = 12;
+    const categoryProduct = 'Laptops';
+    const costProduct = 450000;
+    const totalSumProduct = countProduct * costProduct;
+    
+    console.log('Product name: ', nameProduct);
+    console.log('Total sum: ', totalSumProduct);
+
+}
+
